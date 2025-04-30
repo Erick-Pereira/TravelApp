6864d4588ea137f602f32b39cb32b54159cd9a1b
Erick-Pereira <erickbaptistapereira@gmail.com>
2025-04-29 22:26:19 -0300

    Ajuste migration

--- app/src/main/java/com/example/travelapp/database/AppDatabase.kt
+++ app/src/main/java/com/example/travelapp/database/AppDatabase.kt
@@ -3,9 +3,10 @@ package com.example.travelapp.database
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
-import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.travelapp.dao.TravelDao
 import com.example.travelapp.dao.UserDao
 import com.example.travelapp.entity.Travel
@@ -27,10 +28,21 @@ abstract class AppDatabase : RoomDatabase() {
         private var Instance: AppDatabase? = null
         fun getDatabase(context: Context): AppDatabase {
             return Instance ?: synchronized(this) {
-                Room.databaseBuilder(context, AppDatabase::class.java, "user_database").fallbackToDestructiveMigration()
+                Room.databaseBuilder(context, AppDatabase::class.java, "user_database")
+                    .addMigrations(MIGRATION_1_2)
                     .build()
                     .also { Instance = it }
             }
         }
+    }
+}
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE `travel` " +
+                    "ADD COLUMN IF NOT EXISTS `startDate` DATE NOT NULL DEFAULT CURRENT_DATE," +
+                    "ADD COLUMN IF NOT EXISTS `endDate` DATE NOT NULL DEFAULT CURRENT_DATE"
+        )
     }
 }
