df941cf40ce6409042606e9b7a3c6449473a185b
ALUNOS\erick.pereira <erickbaptistapereira@gmail.com>
2025-04-29 21:10:44 -0300

    Ajuste data

--- app/src/main/java/com/example/travelapp/entity/Travel.kt
+++ app/src/main/java/com/example/travelapp/entity/Travel.kt
@@ -3,15 +3,14 @@ package com.example.travelapp.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.travelapp.enums.EnumTravelType
-import java.time.Instant
-import java.util.Date
+import java.time.LocalDate
 
 @Entity
 data class Travel(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val destination: String = "",
     val travelType: EnumTravelType,
-    val startDate: Date = Date(),
-    val endDate: Date = Date(),
+    val startDate: LocalDate = LocalDate.now(),
+    val endDate: LocalDate = LocalDate.now(),
     val budget: Double = 0.0
 )
